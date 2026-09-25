3e8e47d3df6dad3bb6027033001c1029057498b6
Emiliano Medina Martínez <emimedina.146@gmail.com>
2026-09-25 19:21:28 -0300

    Hecho Clase6.Tema2.Ejercicio1

--- Clase6/Tema2/Ejercicio1/Main.java
+++ Clase6/Tema2/Ejercicio1/Main.java
@@ -3,7 +3,7 @@ package Clase6.Tema2.Ejercicio1;
 public class Main {
 
     public static void main(String[] args) {
-        EmpleadoPorHoras empleado1 = new EmpleadoPorHoras("Emi", 54321, 40, 2000);
+        EmpleadoPorHoras empleado1 = new EmpleadoPorHoras("Emi", 54321, 40, 20000);
         EmpleadoAsalariado empleado2 = new EmpleadoAsalariado("Leonel", 44444, 650000, 10500);
 
         System.out.println("Empleado 1: " + empleado1.getNombre() + ", es un EmpleadoPorHoras");
